2d99371ce049d7278fe8d10cbbf1377f9e4d69d0
Ali nouman <hb6348248@gmail.com>
2023-05-12 14:15:25 +0500

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'es2015',
+    outDir: 'build',
+    minify: 'terser',
+    emptyOutDir: true,
+    sourcemap: true,
+    brotliSize: true,
     rollupOptions: {
-      external: ['axios']
-    }
+      external: ['axios'],
+    },
   },
-})
+});
